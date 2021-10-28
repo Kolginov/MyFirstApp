824a10a74c4cd6a696d90cb752adbec018d87f60
Олег <kolginovog@mailru>
2021-10-28 11:24:40 +0300

    CPC5

--- app/src/main/java/study/kolginov/myapplication/MainActivity.kt
+++ app/src/main/java/study/kolginov/myapplication/MainActivity.kt
@@ -2,10 +2,45 @@ package study.kolginov.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TextPushkin", "Ты видел деву на скале")
+        Log.d("TextPushkin", "В одежде белой над волнами")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TextPushkin", "Когда, бушуя в бурной мгле,")
+        Log.d("TextPushkin", "Играло море с берегами,")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TextPushkin", "Когда луч молний озарял")
+        Log.d("TextPushkin", "Ее всечасно блеском алым")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TextPushkin", "И ветер бился и летал")
+        Log.d("TextPushkin", "С ее летучим покрывалом?")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TextPushkin", "Прекрасно море в бурной мгле")
+        Log.d("TextPushkin", "И небо в блесках без лазури;")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TextPushkin", "Но верь мне: дева на скалее")
+        Log.d("TextPushkin", "Прекрасней волн, небес и бури.")
+    }
+
 }
